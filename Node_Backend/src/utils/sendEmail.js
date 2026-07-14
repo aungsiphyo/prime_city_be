c7d10a6eb5b27a5fad8a4cdeea03f31d28abaaaa
Aung Si Phyo <aungsiphyo@Aungs-MacBook-Air-2.local>
2026-07-14 13:21:29 +0630

    IPv6 to IPv4 change

--- Node_Backend/src/utils/sendEmail.js
+++ Node_Backend/src/utils/sendEmail.js
@@ -2,11 +2,14 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, otp) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    family: 4,
   });
 
   const mailOptions = {
