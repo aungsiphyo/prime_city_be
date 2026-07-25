b4dbc79bfff031e9096bbf5f767bfcc1fd1ddb10
Phyo Myat Min <phyomyatmin646@gmail.com>
2026-07-25 23:15:00 +0630

    fix: increase express body size limit and export sendVoiceMessage

--- Node_Backend/server.js
+++ Node_Backend/server.js
@@ -66,8 +66,8 @@ function getRegistrationFormUrl(req) {
 const sseClients = new Map();
 let nextSseClientId = 1;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(
   cors({
